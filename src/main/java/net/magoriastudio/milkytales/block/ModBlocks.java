41a11244a0c0b4b450db886eb43c18d34e7378ff
Contykpo <samu.1furlanich@gmail.com>
2024-07-17 16:56:38 -0300

    Added soulstone tools.

--- src/main/java/net/magoriastudio/milkytales/block/ModBlocks.java
+++ src/main/java/net/magoriastudio/milkytales/block/ModBlocks.java
@@ -2,10 +2,12 @@ package net.magoriastudio.milkytales.block;
 
 import net.magoriastudio.milkytales.MilkyTales;
 import net.magoriastudio.milkytales.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,9 +21,10 @@ public class ModBlocks
 {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MilkyTales.MOD_ID);
-
     public static final RegistryObject<Block> RAW_SOULSTONE_BLOCK = registerBlock("raw_soulstone_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.DEEPSLATE)));
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.STONE)
+                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)));
+
     public static final RegistryObject<Block> SOULSTONE_BLOCK = registerBlock("soulstone_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.COPPER)));
 
